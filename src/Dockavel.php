11687ff588e37771acc555117f41f02b3a8b3051
Sam Arossi Maosa <smaosa@strathmore.edu>
2021-05-19 08:21:56 +0300

    - Added Support for Laravel Octane Preset (with PHP8) - Change: Changed UID and USER To ARGs with default values of 1000 and 'developer' respectively

--- src/Dockavel.php
+++ src/Dockavel.php
@@ -9,26 +9,27 @@ use Symfony\Component\Process\Process;
 
 class Dockavel extends Command
 {
-    protected $signature='docker:install 
+    protected $signature = 'docker:install 
                          {image : The image name}
                          {network? : The bridge network to use}
                          {--f|force : Whether to overwrite published files}
                         ';
-    protected $description="Run an install to publish all the resources needed to run docker";
+    protected $description = "Run an install to publish all the resources needed to run docker";
 
-    public function handle() {
+    public function handle()
+    {
         $this->info("We are here");
         $force = $this->hasOption('force');
-        $php = $this->choice("Choose your version of PHP",["php8.x","php7.4"], "php8.x");
+        $php = $this->choice("Choose your version of PHP", ["php8.x", "php7.4", "octane"], "php8.x");
         if (!$php) {
             $this->error("You must specify a php version");
             die();
         }
         $image = $this->argument('image');
-        $network = $this->argument('network') ?? $image."net";
+        $network = $this->argument('network') ?? $image . "net";
         $this->prepareEnv();
-        $this->publishConfig($php,$force);
-        if ($network!=='bridge' && $network !=='docker' && $this->confirm("would you like to create the $network bridge network?")) {
+        $this->publishConfig($php, $force);
+        if ($network !== 'bridge' && $network !== 'docker' && $this->confirm("would you like to create the $network bridge network?")) {
             $this->cmd("docker network create $network");
         }
         if ($this->confirm("would you like to create the {$image}db docker volume for the database?")) {
@@ -38,36 +39,40 @@ class Dockavel extends Command
             $this->cleanUp();
         }
     }
-    private function cmd($cmd) {
-        $process = Process::fromShellCommandline($cmd,null,array_merge($_SERVER, $_ENV),null,null);
-        $process->run(function ($type,$line){
+    private function cmd($cmd)
+    {
+        $process = Process::fromShellCommandline($cmd, null, array_merge($_SERVER, $_ENV), null, null);
+        $process->run(function ($type, $line) {
             $this->line($line);
         });
     }
-    protected function replaceInFile($filename, $search, $replace) {
-        $content=file_get_contents($filename);
-        $newContent = str_replace($search,$replace,$content);
+    protected function replaceInFile($filename, $search, $replace)
+    {
+        $content = file_get_contents($filename);
+        $newContent = str_replace($search, $replace, $content);
         file_put_contents($filename, $newContent);
     }
-    protected function prepareEnv() {
+    protected function prepareEnv()
+    {
         $image = $this->argument('image');
         $network = $this->argument('network');
-        $env = __DIR__."/../env/.env.docker";
+        $env = __DIR__ . "/../env/.env.docker";
         $dest = base_path(".env.docker");
         $this->cmd("cp -rf $env $dest");
-        $this->replaceInFile($dest,":image:",$image);
-        $this->replaceInFile($dest,":network:",$network);
+        $this->replaceInFile($dest, ":image:", $image);
+        $this->replaceInFile($dest, ":network:", $network);
     }
-    protected function publishConfig(string $phpVersion, bool $force=false) {
+    protected function publishConfig(string $phpVersion, bool $force = false)
+    {
         $this->info('Publishing docker Config...');
         $this->call('vendor:publish', [
             '--provider' => 'Savannabits\Dockavel\DockavelServiceProvider',
-            '--force' =>$force
+            '--force' => $force
         ]);
 
         //Configure the right version
-        $this->cmd("cp -rf ".base_path("docker/$phpVersion/docker-compose.yml")." ".base_path("."));
-        $this->cmd("cp -rf ".base_path("docker/$phpVersion/.docker")." ".base_path("."));
+        $this->cmd("cp -rf " . base_path("docker/$phpVersion/docker-compose.yml") . " " . base_path("."));
+        $this->cmd("cp -rf " . base_path("docker/$phpVersion/.docker") . " " . base_path("."));
         $this->cmd("rm -rf docker");
     }
 
